5c0fcf1690f0bdee40827e4c8a6979e3e88eda89
刘函峰 <776122767@qq.com>
2020-10-22 16:07:16 +0800

    test super;father

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,14 @@
  * @Date 2020/10/22 14:46
  */
 public class Hello {
+    public Hello(int i) {
+        System.out.println(i);
+    }
+
+    void run(){
+        System.out.println("run");
+    }
+
     public static void main(String[] args) {
 
         System.out.println("hello Lhf");
